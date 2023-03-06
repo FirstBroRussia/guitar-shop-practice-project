0955d9a2b1a7a0e36d375bd44895dd8f903ac1fb
FirstBroRussia <chipanrussia@gmail.com>
2023-03-07 01:37:28 +0300

    Начинаем делать фронтенд

--- guitar-shop-backend/ecosystem.config.js
+++ guitar-shop-backend/ecosystem.config.js
@@ -1,9 +1,34 @@
 module.exports = {
   apps : [
     {
-      script: 'nx run users:serve',
-      name: 'users-microservice',
-      watch: './apps/users',
-    }
+      name: 'users',
+      script: 'start-users.js',
+      watch: './apps/users/',
+    },
+    {
+      name: 'products',
+      script: 'start-products.js',
+      watch: './apps/products/',
+    },
+    {
+      name: 'comments',
+      script: 'start-comments.js',
+      watch: './apps/comments/',
+    },
+    {
+      name: 'orders',
+      script: 'start-orders.js',
+      watch: './apps/orders/',
+    },
+    {
+      name: 'notify',
+      script: 'start-notify.js',
+      watch: './apps/notify/',
+    },
+    {
+      name: 'bff',
+      script: 'start-bff.js',
+      watch: './apps/bff/',
+    },
   ],
 };
